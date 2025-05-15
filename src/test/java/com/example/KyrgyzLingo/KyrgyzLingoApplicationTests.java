1b3a4832bb194c270469c1dae159bc661ab7e759
alimanbazarbekova <aliman.bazarbekova@alatoo.edu.kg>
2025-05-15 13:36:02 +0600

    Push full project

---
+++ src/test/java/com/example/KyrgyzLingo/KyrgyzLingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KyrgyzLingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyrgyzLingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
