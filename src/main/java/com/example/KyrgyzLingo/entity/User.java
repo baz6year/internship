eef70f3097f6f8c32005a342363096405ca817ff
alimanbazarbekova <aliman.bazarbekova@alatoo.edu.kg>
2025-05-19 13:31:33 +0600

    Added annotations in User class

--- src/main/java/com/example/KyrgyzLingo/entity/User.java
+++ src/main/java/com/example/KyrgyzLingo/entity/User.java
@@ -1,12 +1,17 @@
 package com.example.KyrgyzLingo.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
-
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+    @Column(nullable = false, unique = true)
     String username;
+    @Column(nullable = false, unique = true)
     String email;
+    @Column(nullable = false)
     String password;
 }
