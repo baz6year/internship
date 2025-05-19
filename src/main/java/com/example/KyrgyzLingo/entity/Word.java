0f4ca65fcb9ed894cd9120535eb08bdc34971d44
TElzada <turdubekovaelzada@gmail.com>
2025-05-19 13:54:10 +0600

    Added annotations in Word entity

--- src/main/java/com/example/KyrgyzLingo/entity/Word.java
+++ src/main/java/com/example/KyrgyzLingo/entity/Word.java
@@ -19,4 +19,50 @@ public class Word {
     private String transcription;
 
     private String topic;
+
+    public Word() {
+    }
+
+    public Word(String kyrgyzWord, String translation, String transcription, String topic) {
+        this.kyrgyzWord = kyrgyzWord;
+        this.translation = translation;
+        this.transcription = transcription;
+        this.topic = topic;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getKyrgyzWord() {
+        return kyrgyzWord;
+    }
+
+    public void setKyrgyzWord(String kyrgyzWord) {
+        this.kyrgyzWord = kyrgyzWord;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
+
+    public void setTranslation(String translation) {
+        this.translation = translation;
+    }
+
+    public String getTranscription() {
+        return transcription;
+    }
+
+    public void setTranscription(String transcription) {
+        this.transcription = transcription;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
 }
